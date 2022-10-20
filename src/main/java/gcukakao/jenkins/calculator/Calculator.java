cce6ef951d24c3060ff63b6d5e97121be4100758
KIM-JOOHYUN <kjh99723@gmail.com>
2022-10-20 11:50:13 +0900

    test

--- src/main/java/gcukakao/jenkins/calculator/Calculator.java
+++ src/main/java/gcukakao/jenkins/calculator/Calculator.java
@@ -3,8 +3,7 @@ import org.springframework.stereotype.Service;
  
 @Service 
 public class Calculator { 
-     public int sum(int a, int b) { 
-//          dkdkkd
+     public int sum(int a, int b) {
           return a + b;
 
      } 
